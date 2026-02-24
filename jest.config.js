258616af07b9a1e3fed70b5774aa0c9a0c30f2df
lucaskampi <lucasclmaia0@gmail.com>
2026-02-24 01:16:37 -0800

    test: add jest configuration for unit and e2e projects

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,45 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
-  testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
-  },
-  collectCoverageFrom: ['**/*.(t|j)s'],
-  coverageDirectory: '../coverage',
-  testEnvironment: 'node',
-  coveragePathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/',
-    '/prisma/',
-    '.*.module.ts',
-    '.*.controller.ts',
-    'main.ts',
+  projects: [
+    {
+      displayName: 'unit',
+      testEnvironment: 'node',
+      rootDir: '.',
+      testMatch: ['<rootDir>/src/**/*.spec.ts', '<rootDir>/packages/**/*.spec.ts'],
+      transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+      },
+      collectCoverageFrom: [
+        'src/**/*.(t|j)s',
+        'packages/**/*.(t|j)s',
+      ],
+      coverageDirectory: 'coverage/unit',
+      coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/prisma/',
+        'main.ts',
+      ],
+    },
+    {
+      displayName: 'e2e',
+      testEnvironment: 'node',
+      rootDir: 'test',
+      testMatch: ['**/*.e2e-spec.ts'],
+      transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+      },
+      collectCoverageFrom: ['../src/**/*.(t|j)s'],
+      coverageDirectory: '../coverage/e2e',
+      coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/prisma/',
+        'main.ts',
+      ],
+      setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+      moduleNameMapper: {
+        '^@nest-microservices/shared$': '<rootDir>/../packages/shared/src/index.ts',
+      },
+    },
   ],
 }
